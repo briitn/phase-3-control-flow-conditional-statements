7b199e4218c2b888b307a9a6ddbf21b0029add2c
princegobah <babynamek@gmail.com>
2022-05-21 19:07:36 -0400

    commit tree 6588d912b369b304456f41dc83c0dc01e0f9ce20

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,63 @@
+
+
 def admin_login(username, password)
-  # your code here
+
+ if ((username=='ADMIN'||username=='admin') && password=='12345')
+   'Access granted'
+ else 
+  'Access denied'
+ end
+
 end
+
+
 
 def hows_the_weather(temperature)
-  # your code here
+  
+  if ( temperature<40)
+      "It's brisk out there!"
+
+  elsif(temperature>39 &&temperature <66)
+     "It's a little chilly out there!"
+     elsif(temperature>85)
+      "It's too dang hot out there!"
+     else 
+      "It's perfect out there!"
+
+
+  end
+
 end
 
+hows_the_weather(65)
 def fizzbuzz(num)
-  # your code here
+  
+  if (num%3==0 &&num%5!=0)
+    "Fizz"
+    elsif (num%5==0 && num%3!=0)
+      "Buzz"
+    elsif(num%3==0 && num%5==0)
+      "FizzBuzz" 
+    else
+      num 
+    end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+owner=case operation
+when '+' 
+  num1+num2
+when '-' 
+   num1-num2
+when '*'
+  num1*num2
+when '/'
+  num1/num2
+else 
+  puts "Invalid operation!"
+   nil
+ return  owner 
+end
 end
 
+calculator("8", 1,2)
